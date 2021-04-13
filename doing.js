1890af572a5a354bcea87499f430937f2ab4befa
Ethan Skinner <10249884+el-ethan@users.noreply.github.com>
2021-04-13 10:02:57 -0500

    Update doc

--- doing.js
+++ doing.js
@@ -42,7 +42,7 @@ function readEvents(nLines) {
         const limitedEvents = limitEvents(parsedEvents, nLines)
         console.log(limitedEvents.map(formatEvent).join('\n'));
     } else {
-        console.log('You haven\'t done anything yet. Log something you are doing with `doing-now the thing you are doing`')
+        console.log('You haven\'t done anything yet. Log something you are doing with `doing the thing you are doing`')
     }
 }
 
